590b56f1d70b162cd77127cb35670afd6836f4ef
Студент НИУ ВШЭ <Student@POKR-R607-STD07.staff.corp.local>
2024-07-03 11:39:13 +0300

    first update

---
+++ Assets/Scripts/Hero/Hero.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.015f; // �������� ��������
+    [SerializeField] private int lives = 5; // ���-�� hp
+    [SerializeField] private float jumpForce = 9f; // ���� ������
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (isGrounded && Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed = 0.015f);
+
+        sprite.flipX = dir.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.8f);
+        isGrounded = collider.Length > 1;
+    }
+}
