11bb8368d516b3f7bfc8949122b230b8de4be262
Студент НИУ ВШЭ <Student@POKR-R607-STD03.staff.corp.local>
2024-07-11 15:28:43 +0300

    additional files

---
+++ Assets/Scripts/Enemy/Enemy Damage.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public HealthSystem healthSystem;
+    public int damage;
+    public Collider2D WeaponCollider;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        if (collision.otherCollider != WeaponCollider) return;
+        if (collision.gameObject.tag == "Player")
+        {
+            healthSystem.TakeDamage(damage);
+
+            WeaponCollider.gameObject.SetActive(false);
+            WeaponCollider.enabled = false;
+
+            Invoke("EnableCollider", 2f); // Re-enable collider after 1 second
+        }
+    }
+
+    private void EnableCollider()
+    {
+        WeaponCollider.gameObject.SetActive(true);
+        WeaponCollider.enabled = true;
+    }
+}
